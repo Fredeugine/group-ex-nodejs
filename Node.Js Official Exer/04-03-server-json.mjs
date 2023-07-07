74375452e57a22a11b40d208d850ff9b5e852a34
Fred Eugine Adomako <fredadomako2003@gmail.com>
2023-07-07 15:10:37 +0200

    Ex1

---
+++ Node.Js Official Exer/04-03-server-json.mjs
@@ -0,0 +1,19 @@
+import { createServer } from "node:http";
+
+// After making request to 'http://localhost:3000' with curl using the --verbose flag in terminal, the value of the Content-Length was 19
+
+const server = createServer((request, response) => {
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  response.setHeader("Content-Type", "application/json");
+
+  const jsonResponseBody = JSON.stringify({ location: "Mars" });
+
+  response.end(jsonResponseBody);
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
